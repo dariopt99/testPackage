3e127adefed9fa03ee791ee1b65851c62d1d34cd
dariopt99 <dario_pietro_matina@brown.edu>
2025-03-16 23:24:11 -0400

    new change to my theme

--- R/my_theme.R
+++ R/my_theme.R
@@ -9,10 +9,11 @@
 #' 
 #' 
 my_theme <- function() {
-  ggplot2::theme_minimal() +
+   ggplot2::theme_minimal() +
     ggplot2::theme(
-      panel.background = ggplot2::element_blank(),  # Remove background for a clean look
-      axis.title = ggplot2::element_text(size = 14, face = "bold", family = "sans"),  # Use a generic sans-serif font
-      panel.grid.major = ggplot2::element_line(color = "darkblue", linetype = "dotted")  # Change major grid lines to dotted blue lines
+      plot.background = ggplot2::element_rect(fill = "white", color = NA),  # Ensure plot background is white
+      panel.background = ggplot2::element_rect(fill = "white", color = NA),  # Set panel background to white
+      axis.title = ggplot2::element_text(size = 14, face = "bold",color="darkblue"),  
+      panel.grid.major = ggplot2::element_line(color = "darkblue", linetype = "dotted")  
     )
 }
