7d587b1cf83f068f048fd587b61e8ecb49deb933
dariopt99 <dario_pietro_matina@brown.edu>
2025-03-16 23:31:26 -0400

    my theme modified

--- R/my_theme.R
+++ R/my_theme.R
@@ -11,9 +11,9 @@
 my_theme <- function() {
    ggplot2::theme_minimal() +
     ggplot2::theme(
-      plot.background = ggplot2::element_rect(fill = "white", color = NA),  # Ensure plot background is white
-      panel.background = ggplot2::element_rect(fill = "white", color = NA),  # Set panel background to white
-      axis.title = ggplot2::element_text(size = 14, face = "bold",color="darkblue"),  
-      panel.grid.major = ggplot2::element_line(color = "darkblue", linetype = "dotted")  
+      ggplot2::plot.background = ggplot2::element_rect(fill = "white", color = NA),  # Ensure plot background is white
+      ggplot2::panel.background = ggplot2::element_rect(fill = "white", color = NA),  # Set panel background to white
+      ggplot2::axis.title = ggplot2::element_text(size = 14, face = "bold",color="darkblue"),  
+      ggplot2::panel.grid.major = ggplot2::element_line(color = "darkblue", linetype = "dotted")  
     )
 }
